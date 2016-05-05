566bf77246fe221a5342a71b3820467134d385b8
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2016-05-05 18:46:02 -0400

    vis wxl traj data

--- dataConvTools/xml2testTxt.py
+++ dataConvTools/xml2testTxt.py
@@ -6,20 +6,22 @@ import scipy.misc
 import numpy as np
 from utils import readXML
 
-inpath = '/nfs.yoda/rgirdhar/Work/Data2/014_TVShows/processed/Scratch/004_HIMYMFull/011_TrainingData/002_HIMYMData_clust50.h5_2'
-outpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/Fixed/001_HIMYM.txt'
-correctedDir = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/XML/himym/'
 
-def genFromTxt():
+def genFromTxt():  # wxl
   with open(inpath, 'r') as f, open(outpath, 'w') as fout:
-    for lno,line in enumerate(f.read().splitlines()):
-      if lno+1 in cur_labeled:
-        elts = line.split()
-        pose = readXML(os.path.join(correctedDir, str(lno+1) + '.xml'))
-        if pose:
-          fout.write('%s %s %s\n' % (elts[0], ' '.join(pose), elts[-1]))
+    for lno,line in enumerate(f):
+      out_imname = '%08d' % (lno + 1)
+      elts = line.split()
+      for j in range(int(elts[-1])):
+        next(f)
+      poses = readXML(os.path.join(correctedDir, out_imname + '.xml'))
+      if poses is None:
+        poses = []
+      fout.write('%s %s %d\n' % (elts[0], elts[1], len(poses)))
+      for pose in poses:
+        fout.write('%s\n' % (' '.join(pose)))
 
-def genFromH5():
+def genFromH5():  # rohit
   with open(inpath + '.imlist', 'r') as f:
     imgslist = f.read().splitlines()
   with open(inpath + '.mlist', 'r') as f:
@@ -36,4 +38,15 @@ def genFromH5():
           fout.write('%s\n' % (' '.join(pose)))
 
 if __name__ == '__main__':
-  genFromH5()
+  if 0:
+    inpath = '/nfs.yoda/rgirdhar/Work/Data2/014_TVShows/processed/Scratch/004_HIMYMFull/011_TrainingData/002_HIMYMData_clust50.h5_2'
+    outpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/Fixed/001_HIMYM.txt'
+    correctedDir = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/XML/himym/'
+    method = genFromH5
+  elif 1:
+    inpath = '/nfs/ladoga_no_backups/users/xiaolonw/affordance/pairs_216.txt'
+    outpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/Fixed/002_HIMYM_traj.txt'
+    correctedDir = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/004_HIMYMFull/014_ManualFineFix/XML/himym_traj/'
+    method = genFromTxt
+
+  method()
