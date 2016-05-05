f3b5b542ceaa9e8d584fcd172675ce915b0d7a94
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2016-05-05 00:02:57 -0400

    refactor

--- dataConvTools/trainTxt2xml.py
+++ dataConvTools/trainTxt2xml.py
@@ -4,6 +4,7 @@ import h5py
 import subprocess
 import scipy.misc
 import numpy as np
+from utils import genXML, genConcatImg
 
 inpath = 'select_res.txt'
 trainH5 = '002_HIMYMData_clust50.h5_2'
@@ -15,30 +16,6 @@ imOutdir = os.path.join(labelmedir, 'Images/', collectionName); subprocess.call(
 annotOutdir = os.path.join(labelmedir, 'Annotations/', collectionName); subprocess.call('mkdir -p ' + annotOutdir, shell=True)
 listOutpath = os.path.join(labelmedir, 'annotationCache/DirLists/', collectionName + '.txt')
 
-def genXML(poses, fname, foldname):
-  root = Element('annotation')
-  fname_el = SubElement(root, 'filename')
-  fname_el.text = fname
-  foldname_el = SubElement(root, 'folder')
-  foldname_el.text = foldname
-  for pid, pose in enumerate(poses):
-    obj_el = SubElement(root, 'object')
-    obj_el_name = SubElement(obj_el, 'name')
-    obj_el_name.text = 'Pose' + str(pid)
-    obj_el_polygon = SubElement(obj_el, 'polygon')
-    obj_el_polygon_uname = SubElement(obj_el_polygon, 'username')
-    obj_el_polygon_uname.text = 'temp'
-    curpos = 0
-    for i in range(17):
-      obj_el_polygon_pt = SubElement(obj_el_polygon, 'pt')
-      obj_el_polygon_pt_x = SubElement(obj_el_polygon_pt, 'x')
-      obj_el_polygon_pt_x.text = str(pose[curpos])
-      curpos += 1
-      obj_el_polygon_pt_y = SubElement(obj_el_polygon_pt, 'y')
-      obj_el_polygon_pt_y.text = str(pose[curpos])
-      curpos += 1
-  return tostring(root)
-
 def genFromTxt():
   with open(inpath, 'r') as f:
     for lno,line in enumerate(f.read().splitlines()):
@@ -46,13 +23,6 @@ def genFromTxt():
       pose = elts[1:]
       with open(os.path.join(outdir, str(lno+1) + '.xml'), 'w') as fout:
         fout.write(genXML(pose, str(lno+1) + '.jpg', collectionName))
-
-def genConcatImg(qpath, mpath, outpath, GAP=5):
-  Q = scipy.misc.imread(qpath)
-  M = scipy.misc.imread(mpath)
-  M = scipy.misc.imresize(M, (Q.shape[1] * 1.0) / M.shape[1])
-  R = np.concatenate((Q, np.zeros((GAP,Q.shape[1],Q.shape[2])), M), axis=0)
-  scipy.misc.imsave(outpath, R)
 
 def genFromH5():
   with open(trainH5 + '.imlist', 'r') as f:
