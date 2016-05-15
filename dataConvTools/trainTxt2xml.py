821af6aa63fe17d3ae5a19b0b7c4c8f13cc51591
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2016-05-14 23:36:47 -0400

    enhance run for friends

--- dataConvTools/trainTxt2xml.py
+++ dataConvTools/trainTxt2xml.py
@@ -11,15 +11,20 @@ if 0:
   trainH5 = '002_HIMYMData_clust50.h5_2'
   labelmedir = '/mnt/colossus/Work/public_html/Work/Projects/0006_Affordances/0012_PoseLabelTool/LabelMeAnnotationTool/'
   collectionName = 'himym'
-  himymdir = '/mnt/colossus/Work/public_html/Work/Datasets/0006_TVShows/Data/frames/004_HIMYMFull/'
-elif 1:
+  imgsdir = '/mnt/colossus/Work/public_html/Work/Datasets/0006_TVShows/Data/frames/004_HIMYMFull/'
+elif 0:
   trainH5 = 'TBBTData/001_TBBTData.h5'
   labelmedir = '/mnt/colossus/Work/public_html/Work/Projects/0006_Affordances/0012_PoseLabelTool/LabelMeAnnotationTool/'
   collectionName = 'TBBTDataPositive'
-  himymdir = '/mnt/colossus/Work/public_html/Work/Datasets/0006_TVShows/Data/frames/005_TBBT/'
+  imgsdir = '/mnt/colossus/Work/public_html/Work/Datasets/0006_TVShows/Data/frames/005_TBBT/'
+elif 1:
+  trainH5 = 'FriendsData/001_FriendsData.h5'
+  labelmedir = '/mnt/colossus/Work/public_html/Work/Projects/0006_Affordances/0012_PoseLabelTool/LabelMeAnnotationTool/'
+  collectionName = 'FriendsDataPositive'
+  imgsdir = '/mnt/colossus/Work/public_html/Work/Datasets/0006_TVShows/Data/frames/007_Friends/'
 
-imOutdir = os.path.join(labelmedir, 'Images/', collectionName); subprocess.call('rm -r %s && mkdir -p %s'  % (imOutdir, imOutdir), shell=True)
-annotOutdir = os.path.join(labelmedir, 'Annotations/', collectionName); subprocess.call('rm -r %s && mkdir -p %s'  % (annotOutdir, annotOutdir), shell=True)
+imOutdir = os.path.join(labelmedir, 'Images/', collectionName); subprocess.call('rm -r %s; mkdir -p %s'  % (imOutdir, imOutdir), shell=True)
+annotOutdir = os.path.join(labelmedir, 'Annotations/', collectionName); subprocess.call('rm -r %s; mkdir -p %s'  % (annotOutdir, annotOutdir), shell=True)
 listOutpath = os.path.join(labelmedir, 'annotationCache/DirLists/', collectionName + '.txt')
 
 def genFromTxt():
@@ -38,7 +43,7 @@ def genFromH5():
   with h5py.File(trainH5, 'r') as f, open(listOutpath, 'w') as lstOut:
     for imid,imname in enumerate(imgslist):
       out_imname = '%08d' % (imid + 1)
-      I = genConcatImg(os.path.join(himymdir, imname), os.path.join(himymdir, mlist[imid]), os.path.join(imOutdir, out_imname + '.jpg'))
+      I = genConcatImg(os.path.join(imgsdir, imname), os.path.join(imgsdir, mlist[imid]), os.path.join(imOutdir, out_imname + '.jpg'))
       pose = f['pose-label/' + str(imid+1)].value.transpose()
       if pose.ndim == 2:
         poses = pose.reshape((1,-1)).tolist()
@@ -50,6 +55,7 @@ def genFromH5():
       with open(os.path.join(annotOutdir, out_imname + '.xml'), 'w') as fout:
         fout.write(xml)
       lstOut.write('%s,%s\n' % (collectionName, out_imname + '.jpg'))
+  subprocess.call('chmod -R a+w %s' % annotOutdir, shell=True)
 
 if __name__ == '__main__':
   genFromH5()
